652fb754d212d0d818abb6c0066688aeba8072ba
Félix Barbeira <fbarbeira@gmail.com>
2014-12-20 15:58:13 +0100

    Some upgrades and fixes.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Bootstrap for ubuntu and debian.
-# This program only installs puppet.
+# This program makes a safe-upgrade and installs puppet.
 
 set -e
 
 OS=$(lsb_release -c -s)
-REL_FILE="puppetlabs-release-${OS}.deb"
-REPO_DEB_URL="https://apt.puppetlabs.com/${REL_FILE}"
+RELEASE_FILE="puppetlabs-release-${OS}.deb"
+REPO_DEB_URL="https://apt.puppetlabs.com/${RELEASE_FILE}"
 
 if [[ "$EUID" -ne "0" ]]; then
   echo "This script must be run as root." >&2
@@ -14,20 +14,22 @@ if [[ "$EUID" -ne "0" ]]; then
 fi
 
 if [[ $(/usr/bin/dpkg -l) != *puppetlabs* ]]; then
-  if [[ ${OS} -eq 'trusty' ]]; then
-    aptitude purge -y ruby-hiera >/dev/null
-  fi
   echo "Configuring Puppetlabs repository... "
-  wget -qO "/tmp/${REL_FILE}" ${REPO_DEB_URL} 2>/dev/null
-  dpkg -i "/tmp/${REL_FILE}" >/dev/null
-  rm -f "/tmp/${REL_FILE}"
+  wget -qO "/tmp/${RELEASE_FILE}" ${REPO_DEB_URL} >/dev/null
+  dpkg -i "/tmp/${RELEASE_FILE}" >/dev/null
+  rm -f "/tmp/${RELEASE_FILE}"
+
+  echo "Updating system..."
   aptitude update >/dev/null
 
-  echo "Updating system... "
-  aptitude safe-upgrade -y >/dev/null
-
+  echo "Upgrading system... "
+  DEBIAN_FRONTEND=noninteractive aptitude safe-upgrade -q -y >/dev/null
   echo "Installing puppet... "
   aptitude install puppet -y >/dev/null
+
+  # Remove deprecated 'templatedir' var.
+  sed -i '/templatedir=\$confdir\/templates/d' /etc/puppet/puppet.conf
+
 else
   echo "Puppet is already installed."
 fi
